c201e063f1780af7511f4d0684db5c177079fab8
sunbing_vendor <sunbing_vendor@sensetime.com>
2020-11-30 16:30:42 +0800

    Initial commit

---
+++ src/main/java/com/example/demo/controller/SocketTestController.java
@@ -0,0 +1,21 @@
+package com.example.demo.controller;
+
+import com.example.demo.service.Socket;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+@RequestMapping("/bootlearn/socket")
+public class SocketTestController {
+    @Autowired
+    private Socket socket;
+    @GetMapping("/{message}")
+    public void sendMessage(@PathVariable("message") String message) throws IOException {
+        socket.sendMessage("这个是controller 发送的消息："+message);
+    }
+}
